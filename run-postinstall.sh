5c438ac24fd60bc3d845d44745bd0c3a52d543d7
kkovalenko <kostjakovalenko@gmail.com>
2021-11-05 16:44:57 +0200

    Renamed kubeadm-install.sh -> k8s-install.sh

--- run-postinstall.sh
+++ run-postinstall.sh
@@ -9,7 +9,7 @@ sudo apt-get update
 ${POSTINSTALL_DIR}/bashrc-update.sh
 ${POSTINSTALL_DIR}/other.sh
 ${POSTINSTALL_DIR}/docker-install.sh
-${POSTINSTALL_DIR}/kubeadm-install.sh
+${POSTINSTALL_DIR}/k8s-install.sh
 
 if [ ! -z ${NEED_VM} ]
 then
